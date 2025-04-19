f04795a3aac20f0bc0f8532edb0fed189e2b2eed
Nyan Ye Htet <nyanyairhtet@ayainnovations.com>
2025-04-19 19:04:56 +0630

    seed menus, permissions and roles data

--- prisma/seeders/roleSeeder.js
+++ prisma/seeders/roleSeeder.js
@@ -4,10 +4,20 @@ export const seedRoles = async (prismaTx) => {
   await prismaTx.role.createMany({
     data: [
       {
+        name: "ROOT",
+        permissionIds: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 20, 31, 32, 33, 34],
+      },
+      {
         name: "QA_COORDINATOR",
+        permissionIds: [21, 27, 28, 29, 30],
       },
       {
         name: "QA_MANAGER",
+        permissionIds: [17, 18, 19, 21, 22, 31, 32, 33, 34],
+      },
+      {
+        name: "Admin",
+        permissionIds: [20, 23, 24, 25, 26]
       },
     ],
   });
